7e31f3f26695c4624cf808eb56c354f3d89779bc
Mukul Dang <Mukul Dang>
2017-07-24 11:34:39 -0400

    try

--- src/main/java/com/twilio/survey/util/TwiMLUtil.java
+++ src/main/java/com/twilio/survey/util/TwiMLUtil.java
@@ -70,11 +70,11 @@ public class TwiMLUtil {
     }
 
     public static String voiceResponseWithRedirect(String message, String questionUrl) throws TwiMLException {
-    	File f=new File("song.mp3");
-    	Play play = new Play.Builder("https://github.com/mukuldang/play/blob/master/song.mp3").build();
+    	//File f=new File("song.mp3");
+    	//Play play = new Play.Builder("https://github.com/mukuldang/play/blob/master/song.mp3").build();
     	return new VoiceResponse.Builder()
         		//.say(new Say.Builder(message).voice(Voice.MAN).language(Language.EN).build())
-                .play(play)
+                //.play(play)
         		.redirect(new Redirect.Builder().url(questionUrl).method(Method.GET).build())
                 .build()
                 .toXml();
